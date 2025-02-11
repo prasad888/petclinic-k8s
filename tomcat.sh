868a44f7a4fb2cbf43cb1a2b68c661c68fb5a9d9
prasad888 <57856397+prasad888@users.noreply.github.com>
2025-02-11 13:13:35 +0530

    install tomcat

--- tomcat.sh
+++ tomcat.sh
@@ -5,8 +5,10 @@ cd /opt
 rpm -ivh jdk-8u131-linux-x64.rpm  
 #install tomcat
 apt-get install unzip -y
-wget https://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.50/bin/apache-tomcat-9.0.50-windows-x64.zip
-unzip apache-tomcat-9.0.50-windows-x64.zip
+wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.3/bin/apache-tomcat-11.0.3-windows-x64.zip
+
+#wget https://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.50/bin/apache-tomcat-9.0.50-windows-x64.zip
+unzip apache-tomcat-11.0.3-windows-x64.zip
 mv  apache-tomcat-9.0.50 tomcat
 chmod -R 700 tomcat 
 cd tomcat/bin/
